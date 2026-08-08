ab163295e5b4edd98f0ab48b4369d9980dff5ada
Jithu-prajith <prajithshri0908@gmail.com>
2026-08-08 13:58:48 +0530

    Time: 2 ms (92.07%), Space: 47.7 MB (76.68%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -4,10 +4,12 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             if(nums[i]!=0){
                 nums[j]=nums[i];
+            
                 if(i!=j){
                     nums[i]=0;
                 }
-                j++;
+                    j++;
+                
             }
         }
     }
